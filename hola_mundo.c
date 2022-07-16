e547e153b09e9d18a5265285f2288ccdb87b0e2c
javieravellan <jacha067@gmail.com>
2022-07-16 09:15:33 -0500

    Update

--- hola_mundo.c
+++ hola_mundo.c
@@ -14,7 +14,6 @@ void mergesort(double a[], int primero, int ultimo)
 
 void mezcla(double a[], int pri, int medio, int ult)
 {
-<<<<<<< HEAD
     double tmp[M];
     int x, y, z;
     x = z = pri;
@@ -46,67 +45,16 @@ void mezcla(double a[], int pri, int medio, int ult)
     // copia los elementos de tmp a a[]
     for (z = pri; z <= ult; z++) {
         a[z] = tmp[z];
-=======
-    int i, j, k;
-    int n1 = medio - izq + 1;
-    int n2 = dere - medio;
-  
-    /* create temp arrays */
-    int L[n1], R[n2];
-  
-    /* Copy data to temp arrays L[] and R[] */
-    for (i = 0; i < n1; i++)
-        L[i] = a[izq + i];
-    for (j = 0; j < n2; j++)
-        R[j] = a[medio + 1 + j];
-  
-    /* Merge the temp aays back into a[l..r]*/
-    i = 0; // Initial index of first subaay
-    j = 0; // Initial index of second subaay
-    k = izq; // Initial index of merged subaay
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            a[k] = L[i];
-            i++;
-        }
-        else {
-            a[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-  
-    /* Copy the remaining elements of L[], if there
-    are any */
-    while (i < n1) {
-        a[k] = L[i];
-        i++;
-        k++;
-    }
-  
-    /* Copy the remaining elements of R[], if there
-    are any */
-    while (j < n2) {
-        a[k] = R[j];
-        j++;
-        k++;
->>>>>>> 475325e2664663ead3cd5e2fd67563357ad63d5f
     }
 }
 
 // Function Main
 int main() 
 {
-<<<<<<< HEAD
     double a[M] = {5, 2, 1};
     mergesort(a, 0, sizeof(a)/sizeof(a[0]) - 1);
     for (int i = 0; i < M; i++)
         printf("Valor %f\n", a[i]);
-=======
-    double a[] = {5, 2, 1, 4};
-    mergesort(a, 0, sizeof(a)/sizeof(a[0]));
-    printf("Indice %f", a[0]);
->>>>>>> 475325e2664663ead3cd5e2fd67563357ad63d5f
 
     return 0;
 }
