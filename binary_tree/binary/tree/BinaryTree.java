b1e8031d5f877c162e681d05a6be0c172b37f3c2
Javier Chiquito Avellán <jacha067@gmail.com>
2024-02-13 22:57:22 -0500

    Add delete operation

--- binary_tree/binary/tree/BinaryTree.java
+++ binary_tree/binary/tree/BinaryTree.java
@@ -18,14 +18,17 @@ public class BinaryTree<T extends Number> {
         return containsNodeRecursive(root, value);
     }
 
+    public void deleteNode(T value) {
+        root = deleteRecursive(root, value);
+    }
     private Node<T> addRecursive(Node<T> currentNode, T value) {
         if (currentNode == null) {
             return new Node<T>(value);
         }
 
-        if (value.byteValue() < currentNode.getData().byteValue()) {
+        if (value.doubleValue() < currentNode.getData().doubleValue()) {
             currentNode.setLeft(addRecursive(currentNode.getLeft(), value));
-        } else if (value.byteValue() > currentNode.getData().byteValue()) {
+        } else if (value.doubleValue() > currentNode.getData().doubleValue()) {
             currentNode.setRight(addRecursive(currentNode.getRight(), value));
         }
 
@@ -35,11 +38,56 @@ public class BinaryTree<T extends Number> {
     private boolean containsNodeRecursive(Node<T> current, T value) {
         if (current == null) return false;
 
-        if (value.byteValue() == current.getData().byteValue()) {
+        if (value.doubleValue() == current.getData().doubleValue()) {
             return true;
         }
 
-        return value.byteValue() < current.getData().byteValue() ?
+        return value.doubleValue() < current.getData().doubleValue() ?
                 containsNodeRecursive(current.getLeft(), value) : containsNodeRecursive(current.getRight(), value);
+    }
+
+    private Node<T> deleteRecursive(Node<T> current, T value) {
+        if (current == null) {
+            return null;
+        }
+        System.out.println("Valor a eliminar: " + value.doubleValue());
+        System.out.println("Valor del nodo actual: " + current.getData().doubleValue());
+        if (value.doubleValue() == current.getData().doubleValue()) {
+            // Si es un Nodo hoja
+            if (current.getLeft() == null && current.getRight() == null) {
+                return null;
+            }
+
+            // Si solo tiene hijo izquierdo
+            if (current.getRight() == null) {
+                return current.getLeft();
+            }
+
+            // Si solo tiene hijo derecho
+            if (current.getLeft() == null) {
+                return current.getRight();
+            }
+
+            // Eliminar Nodo cuando el padre tiene 2 hijos.
+            // Buscamos el nodo màs pequeño del subarbol derecho.
+            var smallValue = findSmallestValue(current.getRight());
+            // seteamos el valor más pequeño del subarbol derecho en el nodo actual, o sea el padre.
+            current.setData(smallValue);
+            // finalmente, eliminamos el nodo y retornamos current.
+            current.setRight(deleteRecursive(current.getRight(), smallValue));
+            return current;
+        }
+
+        if (value.doubleValue() < current.getData().doubleValue()) {
+            current.setLeft(deleteRecursive(current.getLeft(), value));
+            return current;
+        }
+
+        current.setRight(deleteRecursive(current.getRight(), value));
+        return current;
+    }
+
+    private T findSmallestValue(Node<T> node) {
+        return node.getLeft() == null ? node.getData() : findSmallestValue(node.getLeft());
     }
 }
